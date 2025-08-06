4efaf3d22e0391002106f79c7b022764be998a0a
DarwinLeguisamo <128190699+DarwinLeguisamo@users.noreply.github.com>
2025-08-05 22:23:03 -0300

    Update Funciones.ino

--- Funciones.ino
+++ Funciones.ino
@@ -114,13 +114,22 @@ void Brazo(int k, int t) {
   }
 }
 
-void Distancia() {
-  long t;
-  digitalWrite(Trigger, HIGH);
+//  en esta función solo hay que indicar dónde conectamos el ecco y el trigger
+
+int distancia(int TriggerPin, int EchoPin) { 
+  long duration, distanceCm;
+
+  digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+  delayMicroseconds(4);
+  digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
   delayMicroseconds(10);
-  digitalWrite(Trigger,LOW);
-  t = pulseIn(Echo, HIGH);
-  dis = t/59;
+  digitalWrite(TriggerPin, LOW);
+
+  duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+
+  distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+  return distanceCm;
+}
 }
 
 void GirarVerde(int L) {
@@ -210,3 +219,4 @@ void Esquivar(int L) {
   Enviar();
   delay(200);
 }
+
